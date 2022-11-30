c2f39a6c97f2eb4b349d4d822824b67d94fc32dd
Fabian <105529866+fabiancardenas1@users.noreply.github.com>
2022-11-30 18:34:31 -0500

    Initial commit

---
+++ archivo.js
@@ -0,0 +1,7 @@
+const PEPEITOCARRUSEL = document.querySelector('#carouselExampleIndicators')
+const carousel = new bootstrap.Carousel(PEPEITOCARRUSEL, {
+    interval: 2000,
+    pause:false,
+    ride:"carousel"
+})
+
